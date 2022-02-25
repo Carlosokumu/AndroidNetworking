6dce8dd8a02e4772e63f9f0d3fca3d0fa6a85e38
Carlosokumu <69672450+Carlosokumu@users.noreply.github.com>
2022-02-25 17:15:49 +0300

    Auth Interceptor

--- app/src/main/java/com/example/androidnetworking/ui/MainActivity.kt
+++ app/src/main/java/com/example/androidnetworking/ui/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.androidnetworking.ui
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.example.androidnetworking.R
@@ -8,7 +9,7 @@ import com.example.androidnetworking.R
 
 class MainActivity : AppCompatActivity() {
 
-
+    private val viewModel by viewModels<MainViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
