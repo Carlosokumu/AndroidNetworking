cecf348c661c006d4b536cbaa76b53f9524efeff
Carlosokumu <69672450+Carlosokumu@users.noreply.github.com>
2022-02-25 16:48:42 +0300

    Clean Up

--- app/src/main/java/com/example/androidnetworking/ui/MainActivity.kt
+++ app/src/main/java/com/example/androidnetworking/ui/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.example.androidnetworking.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.example.androidnetworking.R
 
 
@@ -12,5 +13,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Toolbar>(R.id.toolbar).inflateMenu(R.menu.menu_search)
+
     }
 }
